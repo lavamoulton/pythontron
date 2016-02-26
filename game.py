8f6bbf2337e77dca00cad5a0d3495f1e0c2b114d
Daniel Moulton <danielzmoulton@gmail.com>
2016-02-25 23:11:23 -0500

    Added help menu

--- game.py
+++ game.py
@@ -118,6 +118,7 @@ def genGrid():
             grid[x].append(False)
     return grid
 
+#sets all buttons to be non clickable
 def resetButtons():
     for button in buttonList:
         button.resetClick()
@@ -131,6 +132,7 @@ def displayMenu():
             button.drawButton(GRID_DISPLAY)
             button.setClick()
 
+#Draws text present in the main menu, not including the buttons
 def drawMenuText():
     menuFont = TITLE_FONT.render("PyTron!", 1, WHITE)
     centerText = menuFont.get_rect()
@@ -143,8 +145,96 @@ def drawMenuText():
     centerText.centery = SCREEN_HEIGHT * .25
     GRID_DISPLAY.blit(morePlayers, centerText)
 
+#Draws text on the help menu, including controls for all potential players
 def displayHelp():
-    pass
+
+    #All text for player 1
+    p1Font = HELP_FONT.render("Player 1 Controls:", 1, WHITE)
+    p1Left = HELP_FONT.render("Left: Left Arrow Key", 1, WHITE)
+    p1Right = HELP_FONT.render("Right: Right Arrow Key", 1, WHITE)
+    p1Up = HELP_FONT.render("Up: Up Arrow Key", 1, WHITE)
+    p1Down = HELP_FONT.render("Down: Down Arrow Key", 1, WHITE)
+
+    centerText = p1Font.get_rect()
+    centerText.centerx = SCREEN_WIDTH * .15
+    centerText.centery = SCREEN_HEIGHT * .1
+    GRID_DISPLAY.blit(p1Font, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery+(SCREEN_HEIGHT*.15)
+    GRID_DISPLAY.blit(p1Left, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery+(SCREEN_HEIGHT*.1)
+    GRID_DISPLAY.blit(p1Right, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery+(SCREEN_HEIGHT*.1)
+    GRID_DISPLAY.blit(p1Up, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery+(SCREEN_HEIGHT*.1)
+    GRID_DISPLAY.blit(p1Down, centerText)
+
+    #All text for player 2
+    p2Font = HELP_FONT.render("Player 2 Controls:", 1, WHITE)
+    p2Left = HELP_FONT.render("Left: A", 1, WHITE)
+    p2Right = HELP_FONT.render("Right: D", 1, WHITE)
+    p2Up = HELP_FONT.render("Up: W", 1, WHITE)
+    p2Down = HELP_FONT.render("Down: S", 1, WHITE)
+
+    centerText = pygame.Rect.copy(centerText)
+    centerText = p2Font.get_rect()
+    centerText.centerx = SCREEN_WIDTH * .5
+    centerText.centery = SCREEN_HEIGHT * .1
+    GRID_DISPLAY.blit(p2Font, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery + (SCREEN_HEIGHT * .15)
+    GRID_DISPLAY.blit(p2Left, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery + (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p2Right, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery + (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p2Up, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery + (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p2Down, centerText)
+
+    #draws dividing line between the first and second set of instructions
+    pygame.draw.line(GRID_DISPLAY, WHITE, (centerText.left-15, SCREEN_HEIGHT * .08),
+                     (centerText.left-15, SCREEN_HEIGHT*.6))
+
+    #All text for player 3
+    p3Font = HELP_FONT.render("Player 3 Controls:", 1, WHITE)
+    p3Left = HELP_FONT.render("Left: J", 1, WHITE)
+    p3Right = HELP_FONT.render("Right: L", 1, WHITE)
+    p3Up = HELP_FONT.render("Up: I", 1, WHITE)
+    p3Down = HELP_FONT.render("Down: K", 1, WHITE)
+
+    centerText = pygame.Rect.copy(centerText)
+    centerText = p3Font.get_rect()
+    centerText.centerx = SCREEN_WIDTH * .85
+    centerText.centery = SCREEN_HEIGHT * .1
+    GRID_DISPLAY.blit(p3Font, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery + (SCREEN_HEIGHT*.15)
+    GRID_DISPLAY.blit(p3Left, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery + (SCREEN_HEIGHT*.1)
+    GRID_DISPLAY.blit(p3Right, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery + (SCREEN_HEIGHT*.1)
+    GRID_DISPLAY.blit(p3Up, centerText)
+    centerText = pygame.Rect.copy(centerText)
+    centerText.centery = centerText.centery + (SCREEN_HEIGHT*.1)
+    GRID_DISPLAY.blit(p3Down, centerText)
+
+    #draws divide line between the 2nd and 3rd set of instructions
+    pygame.draw.line(GRID_DISPLAY, WHITE, (centerText.left-15, SCREEN_HEIGHT*.08),
+                     (centerText.left-15, SCREEN_HEIGHT*.6))
+
+    #makes the back button visible
+    for button in buttonList:
+        if button.info == "Back":
+            button.drawButton(GRID_DISPLAY)
+            button.setClick()
 
 def createButtons():
     buttonList = []
