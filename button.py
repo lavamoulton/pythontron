2864956e3134ab5eb5e8560fe5b77212f0b0a77f
Daniel Moulton <danielzmoulton@gmail.com>
2016-02-27 17:03:59 -0500

    Fixed quality of life issues, mainly formatting and naming conventions

--- button.py
+++ button.py
@@ -1,33 +1,37 @@
+# imports
 import pygame
-from pygame.locals import *
 
 
-# Button object
 class Button(object):
+    """a button object, containing the following:
+        the rectangle it is contained in
+        the text present in it
+        the color of the text
+        the font of the text"""
     # gotta initialize it
-    def __init__(self, inRect, text, color, font):
-        self.buttonRect = inRect
-        self.buttonFont = font.render(text, 1, (230, 230, 230))
+    def __init__(self, in_rect, text, color, font):
+        self.button_rect = in_rect
+        self.button_font = font.render(text, 1, (230, 230, 230))
         self.color = color
         self.info = text
-        self.centerText = self.buttonFont.get_rect()
-        self.centerText.centerx = self.buttonRect.centerx
-        self.centerText.centery = self.buttonRect.centery
+        self.center_text = self.button_font.get_rect()
+        self.center_text.centerx = self.button_rect.centerx
+        self.center_text.centery = self.button_rect.centery
         self.click = False
 
-    def drawButton(self, GRID_DISPLAY):
-        pygame.draw.rect(GRID_DISPLAY, self.color, self.buttonRect, 2)
-        GRID_DISPLAY.blit(self.buttonFont, self.centerText)
+    def draw_button(self, display):
+        pygame.draw.rect(display, self.color, self.button_rect, 2)
+        display.blit(self.button_font, self.center_text)
 
-    def checkClick(self, posX, posY):
+    def check_click(self, pos_x, pos_y):
         if self.click:
-            if posX > self.buttonRect.left and posX < self.buttonRect.right:
-                if posY > self.buttonRect.top and posY < self.buttonRect.bottom:
-                    return True
+            if self.button_rect.left < pos_x < self.button_rect.right \
+                    and self.button_rect.top < pos_y < self.button_rect.bottom:
+                return True
         return False
 
-    def resetClick(self):
+    def reset_click(self):
         self.click = False
 
-    def setClick(self):
+    def set_click(self):
         self.click = True
--- game.py
+++ game.py
@@ -1,11 +1,9 @@
-import sys, pygame
-from pygame.locals import *
-
-#local imports
-import button
+# necessary imports
 from button import *
+from pygame import *
+import sys
 
-#initial vars
+'''initial vars'''
 GAME_CLOCK = 30
 SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 1024
@@ -14,103 +12,114 @@ GRID_HEIGHT = 128
 BOX_WIDTH = SCREEN_WIDTH / GRID_WIDTH
 BOX_HEIGHT = SCREEN_HEIGHT / GRID_HEIGHT
 
-#colors, colors everywhere
+'''colors, colors everywhere'''
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (192, 192, 192)
 BLUE = (0, 0, 255)
 RED = (255, 0, 0)
 
+
 def main():
-    #this game has got to start somewhere, let's initialize some things
+    """main function, contains the main game loop"""
+
+    # this game has got to start somewhere, let's initialize some things
     pygame.init()
     global FPS_CLOCK
     global GRID_DISPLAY
     global TITLE_FONT
     global BUTTON_FONT
     global HELP_FONT
-    global buttonList
+    global button_list
 
-    #create some fonts for later
+    # create some fonts for later
     TITLE_FONT = pygame.font.SysFont("monospace", SCREEN_WIDTH / 15)
     BUTTON_FONT = pygame.font.SysFont("monospace", SCREEN_WIDTH / 20)
     HELP_FONT = pygame.font.SysFont("monospace", SCREEN_WIDTH / 40)
 
-    #and now some important vars
+    # and now some important vars
     FPS_CLOCK = pygame.time.Clock()
     GRID_DISPLAY = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    gameStatus = "Menu"
-    numPlayers = 0
-    gameGrid = genGrid() #initializes 2D array representing the grid
-    buttonList = createButtons() #creates all necessary buttons
+    game_status = "Menu"
+    num_players = 0
+    game_grid = gen_grid()  # initializes 2D array representing the grid
+    button_list = create_buttons()  # creates all necessary buttons
 
-    #misc
+    # misc
     pygame.display.set_caption("PyTron!")
 
-    #it's time to loop!
+    # it's time to loop!
     while True:
-        #gotta reset some stuff each time
+        # gotta reset some stuff each time
         GRID_DISPLAY.fill(BLACK)
-        resetButtons()
+        reset_buttons()
 
-        #what state are we in?
-        if gameStatus == "Menu":
-            displayMenu()
-        elif gameStatus == "Help":
-            displayHelp()
-        elif gameStatus == "Playing":
+        # what state are we in?
+        if game_status == "Menu":
+            display_menu()
+        elif game_status == "Help":
+            display_help()
+        elif game_status == "Playing":
             pass
-        elif gameStatus == "Paused":
+        elif game_status == "Paused":
             pass
 
-        #the user can do things too, right? let's check those events
-        for event in pygame.event.get():
-            if event.type == QUIT:
+        # the user can do things too, right? let's check those events
+        for game_event in pygame.event.get():
+
+            # I don't know why'd they ever quit, but let's give them the option
+            if game_event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == MOUSEBUTTONDOWN:
-                posX, posY = event.pos
-                for button in buttonList:
-                    if button.checkClick(posX, posY):
+
+            # mouse is clicked, check if it is pressing a button
+            elif game_event.type == MOUSEBUTTONDOWN:
+                pos_x, pos_y = game_event.pos
+                for button in button_list:
+                    if button.check_click(pos_x, pos_y):
                         if button.info == "Help":
-                            gameStatus = "Help"
+                            game_status = "Help"
                         elif button.info == "1":
-                            numPlayers = 1
-                            gameStatus = "Playing"
+                            num_players = 1
+                            game_status = "Playing"
                         elif button.info == "2":
-                            numPlayers = 2
-                            gameStatus = "Playing"
+                            num_players = 2
+                            game_status = "Playing"
                         elif button.info == "3":
-                            numPlayers = 3
-                            gameStatus = "Playing"
+                            num_players = 3
+                            game_status = "Playing"
                         elif button.info == "Back":
-                            gameStatus = "Menu"
+                            game_status = "Menu"
                         elif button.info == "Continue":
-                            gameStatus = "Playing"
+                            game_status = "Playing"
                         elif button.info == "Reset":
                             pass
 
-            elif event.type == KEYDOWN and gameStatus == "Playing":
-                if event.key == K_UP:
-                    #Player1.setDirection('up')
+            elif game_event.type == KEYDOWN and game_status == "Playing":
+                if game_event.key == K_UP:
+                    # Player1.setDirection('up')
                     pass
-                elif event.key == K_DOWN:
-                    #Player1.setDirection('down')
+                elif game_event.key == K_DOWN:
+                    # Player1.setDirection('down')
                     pass
-                elif event.key == K_LEFT:
-                    #Player1.setDirection('left')
+                elif game_event.key == K_LEFT:
+                    # Player1.setDirection('left')
                     pass
-                elif event.key == K_RIGHT:
-                    #Player1.setDirection('right')
+                elif game_event.key == K_RIGHT:
+                    # Player1.setDirection('right')
                     pass
-                elif event.key == K_SPACE:
-                    gameStatus = "Paused"
+                elif game_event.key == K_SPACE:
+                    game_status = "Paused"
 
+        # draw current state of screen
         pygame.display.update()
+
+        # tick tock tick tock
         FPS_CLOCK.tick(GAME_CLOCK)
 
-#creates a 2D array representing the grid
-def genGrid():
+
+def gen_grid():
+    """creates a 2D array representing the grid"""
     grid = []
     for x in range(0, GRID_WIDTH):
         grid.append([])
@@ -118,171 +127,175 @@ def genGrid():
             grid[x].append(False)
     return grid
 
-#sets all buttons to be non clickable
-def resetButtons():
-    for button in buttonList:
-        button.resetClick()
 
-#Functions to display the main menu and activate relevant buttons
-def displayMenu():
-    drawMenuText()
+def reset_buttons():
+    """sets all buttons to be non clickable"""
+    for button in button_list:
+        button.reset_click()
 
-    for button in buttonList:
+
+def display_menu():
+    """Functions to display the main menu and activate relevant buttons"""
+    draw_menu_text()
+
+    for button in button_list:
         if button.info != "Back" and button.info != "Continue" and button.info != "Reset":
-            button.drawButton(GRID_DISPLAY)
-            button.setClick()
+            button.draw_button(GRID_DISPLAY)
+            button.set_click()
 
-#Draws text present in the main menu, not including the buttons
-def drawMenuText():
-    menuFont = TITLE_FONT.render("PyTron!", 1, WHITE)
-    centerText = menuFont.get_rect()
-    centerText.centerx = GRID_DISPLAY.get_rect().centerx
-    GRID_DISPLAY.blit(menuFont, centerText)
 
-    morePlayers = TITLE_FONT.render("Number of Players:", 1, WHITE)
-    centerText = morePlayers.get_rect()
-    centerText.centerx = GRID_DISPLAY.get_rect().centerx
-    centerText.centery = SCREEN_HEIGHT * .25
-    GRID_DISPLAY.blit(morePlayers, centerText)
+def draw_menu_text():
+    """Draws text present in the main menu, not including the buttons"""
+    menu_font = TITLE_FONT.render("PyTron!", 1, WHITE)
+    center_text = menu_font.get_rect()
+    center_text.centerx = GRID_DISPLAY.get_rect().centerx
+    GRID_DISPLAY.blit(menu_font, center_text)
 
-#Draws text on the help menu, including controls for all potential players
-def displayHelp():
+    more_players = TITLE_FONT.render("Number of Players:", 1, WHITE)
+    center_text = more_players.get_rect()
+    center_text.centerx = GRID_DISPLAY.get_rect().centerx
+    center_text.centery = SCREEN_HEIGHT * .25
+    GRID_DISPLAY.blit(more_players, center_text)
 
-    #All text for player 1
-    p1Font = HELP_FONT.render("Player 1 Controls:", 1, WHITE)
-    p1Left = HELP_FONT.render("Left: Left Arrow Key", 1, WHITE)
-    p1Right = HELP_FONT.render("Right: Right Arrow Key", 1, WHITE)
-    p1Up = HELP_FONT.render("Up: Up Arrow Key", 1, WHITE)
-    p1Down = HELP_FONT.render("Down: Down Arrow Key", 1, WHITE)
 
-    centerText = p1Font.get_rect()
-    centerText.centerx = SCREEN_WIDTH * .15
-    centerText.centery = SCREEN_HEIGHT * .1
-    GRID_DISPLAY.blit(p1Font, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery+(SCREEN_HEIGHT*.15)
-    GRID_DISPLAY.blit(p1Left, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery+(SCREEN_HEIGHT*.1)
-    GRID_DISPLAY.blit(p1Right, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery+(SCREEN_HEIGHT*.1)
-    GRID_DISPLAY.blit(p1Up, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery+(SCREEN_HEIGHT*.1)
-    GRID_DISPLAY.blit(p1Down, centerText)
+def display_help():
+    """Draws text on the help menu, including controls for all potential players"""
 
-    #All text for player 2
-    p2Font = HELP_FONT.render("Player 2 Controls:", 1, WHITE)
-    p2Left = HELP_FONT.render("Left: A", 1, WHITE)
-    p2Right = HELP_FONT.render("Right: D", 1, WHITE)
-    p2Up = HELP_FONT.render("Up: W", 1, WHITE)
-    p2Down = HELP_FONT.render("Down: S", 1, WHITE)
+    # All text for player 1
+    p1_font = HELP_FONT.render("Player 1 Controls:", 1, WHITE)
+    p1_left = HELP_FONT.render("Left: Left Arrow Key", 1, WHITE)
+    p1_right = HELP_FONT.render("Right: Right Arrow Key", 1, WHITE)
+    p1_up = HELP_FONT.render("Up: Up Arrow Key", 1, WHITE)
+    p1_down = HELP_FONT.render("Down: Down Arrow Key", 1, WHITE)
 
-    centerText = pygame.Rect.copy(centerText)
-    centerText = p2Font.get_rect()
-    centerText.centerx = SCREEN_WIDTH * .5
-    centerText.centery = SCREEN_HEIGHT * .1
-    GRID_DISPLAY.blit(p2Font, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery + (SCREEN_HEIGHT * .15)
-    GRID_DISPLAY.blit(p2Left, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery + (SCREEN_HEIGHT * .1)
-    GRID_DISPLAY.blit(p2Right, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery + (SCREEN_HEIGHT * .1)
-    GRID_DISPLAY.blit(p2Up, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery + (SCREEN_HEIGHT * .1)
-    GRID_DISPLAY.blit(p2Down, centerText)
+    center_text = p1_font.get_rect()
+    center_text.centerx = SCREEN_WIDTH * .15
+    center_text.centery = SCREEN_HEIGHT * .1
+    GRID_DISPLAY.blit(p1_font, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .15)
+    GRID_DISPLAY.blit(p1_left, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p1_right, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p1_up, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p1_down, center_text)
 
-    #draws dividing line between the first and second set of instructions
-    pygame.draw.line(GRID_DISPLAY, WHITE, (centerText.left-15, SCREEN_HEIGHT * .08),
-                     (centerText.left-15, SCREEN_HEIGHT*.6))
+    # All text for player 2
+    p2_font = HELP_FONT.render("Player 2 Controls:", 1, WHITE)
+    p2_left = HELP_FONT.render("Left: A", 1, WHITE)
+    p2_right = HELP_FONT.render("Right: D", 1, WHITE)
+    p2_up = HELP_FONT.render("Up: W", 1, WHITE)
+    p2_down = HELP_FONT.render("Down: S", 1, WHITE)
 
-    #All text for player 3
-    p3Font = HELP_FONT.render("Player 3 Controls:", 1, WHITE)
-    p3Left = HELP_FONT.render("Left: J", 1, WHITE)
-    p3Right = HELP_FONT.render("Right: L", 1, WHITE)
-    p3Up = HELP_FONT.render("Up: I", 1, WHITE)
-    p3Down = HELP_FONT.render("Down: K", 1, WHITE)
+    center_text = p2_font.get_rect()
+    center_text.centerx = SCREEN_WIDTH * .5
+    center_text.centery = SCREEN_HEIGHT * .1
+    GRID_DISPLAY.blit(p2_font, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .15)
+    GRID_DISPLAY.blit(p2_left, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p2_right, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p2_up, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p2_down, center_text)
 
-    centerText = pygame.Rect.copy(centerText)
-    centerText = p3Font.get_rect()
-    centerText.centerx = SCREEN_WIDTH * .85
-    centerText.centery = SCREEN_HEIGHT * .1
-    GRID_DISPLAY.blit(p3Font, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery + (SCREEN_HEIGHT*.15)
-    GRID_DISPLAY.blit(p3Left, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery + (SCREEN_HEIGHT*.1)
-    GRID_DISPLAY.blit(p3Right, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery + (SCREEN_HEIGHT*.1)
-    GRID_DISPLAY.blit(p3Up, centerText)
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = centerText.centery + (SCREEN_HEIGHT*.1)
-    GRID_DISPLAY.blit(p3Down, centerText)
+    # draws dividing line between the first and second set of instructions
+    pygame.draw.line(GRID_DISPLAY, WHITE, (center_text.left - 15, SCREEN_HEIGHT * .08),
+                     (center_text.left - 15, SCREEN_HEIGHT * .6))
 
-    #draws divide line between the 2nd and 3rd set of instructions
-    pygame.draw.line(GRID_DISPLAY, WHITE, (centerText.left-15, SCREEN_HEIGHT*.08),
-                     (centerText.left-15, SCREEN_HEIGHT*.6))
+    # All text for player 3
+    p3_font = HELP_FONT.render("Player 3 Controls:", 1, WHITE)
+    p3_left = HELP_FONT.render("Left: J", 1, WHITE)
+    p3_right = HELP_FONT.render("Right: L", 1, WHITE)
+    p3_up = HELP_FONT.render("Up: I", 1, WHITE)
+    p3_down = HELP_FONT.render("Down: K", 1, WHITE)
 
-    #makes the back button visible
-    for button in buttonList:
+    center_text = p3_font.get_rect()
+    center_text.centerx = SCREEN_WIDTH * .85
+    center_text.centery = SCREEN_HEIGHT * .1
+    GRID_DISPLAY.blit(p3_font, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .15)
+    GRID_DISPLAY.blit(p3_left, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p3_right, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p3_up, center_text)
+    center_text.centery += (SCREEN_HEIGHT * .1)
+    GRID_DISPLAY.blit(p3_down, center_text)
+
+    # draws divide line between the 2nd and 3rd set of instructions
+    pygame.draw.line(GRID_DISPLAY, WHITE, (center_text.left - 15, SCREEN_HEIGHT * .08),
+                     (center_text.left - 15, SCREEN_HEIGHT * .6))
+
+    # makes the back button visible
+    for button in button_list:
         if button.info == "Back":
-            button.drawButton(GRID_DISPLAY)
-            button.setClick()
+            button.draw_button(GRID_DISPLAY)
+            button.set_click()
 
-def createButtons():
-    buttonList = []
 
-    tempFont = TITLE_FONT.render("test", 1, WHITE)
-    centerText = tempFont.get_rect()
-    centerText.centery = SCREEN_HEIGHT * .75
-    centerText.width = SCREEN_WIDTH * .5
-    centerText.centerx = SCREEN_WIDTH * .5
-    helpButton = Button(centerText, "Help", GRAY, BUTTON_FONT)
-    buttonList.append(helpButton)
+def create_buttons():
+    """creates ALL buttons used in the various menus in game
 
-    centerText = pygame.Rect.copy(centerText)
-    centerText.left = SCREEN_WIDTH * .27
-    centerText.width = SCREEN_WIDTH * .12
-    centerText.top = SCREEN_HEIGHT * .5
-    centerText.height = SCREEN_HEIGHT * .1
-    oneButton = Button(centerText, "1", GRAY, BUTTON_FONT)
-    buttonList.append(oneButton)
+        various methods activate the click capabilities and visuals of the buttons"""
+    all_button = []
 
-    centerText = pygame.Rect.copy(centerText)
-    centerText.left = SCREEN_WIDTH * .44
-    twoButton = Button(centerText, "2", GRAY, BUTTON_FONT)
-    buttonList.append(twoButton)
+    # help button
+    temp_font = TITLE_FONT.render("test", 1, WHITE)
+    center_text = temp_font.get_rect()
+    center_text.centery = SCREEN_HEIGHT * .75
+    center_text.width = SCREEN_WIDTH * .5
+    center_text.centerx = SCREEN_WIDTH * .5
+    help_button = Button(center_text, "Help", GRAY, BUTTON_FONT)
+    all_button.append(help_button)
 
-    centerText = pygame.Rect.copy(centerText)
-    centerText.left = SCREEN_WIDTH * .6
-    threeButton = Button(centerText, "3", GRAY, BUTTON_FONT)
-    buttonList.append(threeButton)
+    # 1 player button
+    center_text = pygame.Rect.copy(center_text)
+    center_text.left = SCREEN_WIDTH * .27
+    center_text.width = SCREEN_WIDTH * .12
+    center_text.top = SCREEN_HEIGHT * .5
+    center_text.height = SCREEN_HEIGHT * .1
+    one_button = Button(center_text, "1", GRAY, BUTTON_FONT)
+    all_button.append(one_button)
 
-    centerText = pygame.Rect.copy(centerText)
-    centerText.width = SCREEN_WIDTH * .25
-    centerText.centerx = GRID_DISPLAY.get_rect().centerx
-    centerText.centery = SCREEN_HEIGHT * .8
-    backButton = Button(centerText, "Back", GRAY, BUTTON_FONT)
-    buttonList.append(backButton)
+    # 2 player button
+    center_text = pygame.Rect.copy(center_text)
+    center_text.left = SCREEN_WIDTH * .44
+    two_button = Button(center_text, "2", GRAY, BUTTON_FONT)
+    all_button.append(two_button)
 
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = SCREEN_HEIGHT*.75
-    centerText.centerx = SCREEN_WIDTH*.5
-    pauseButton = Button(centerText, "Continue", GRAY, BUTTON_FONT)
-    buttonList.append(pauseButton)
+    # 3 player button
+    center_text = pygame.Rect.copy(center_text)
+    center_text.left = SCREEN_WIDTH * .6
+    three_button = Button(center_text, "3", GRAY, BUTTON_FONT)
+    all_button.append(three_button)
 
-    centerText = pygame.Rect.copy(centerText)
-    centerText.centery = SCREEN_HEIGHT*.88
-    centerText.centerx = SCREEN_WIDTH*.5
-    resetButton = Button(centerText, "Reset", GRAY, BUTTON_FONT)
-    buttonList.append(resetButton)
-    return buttonList
+    # back button
+    center_text = pygame.Rect.copy(center_text)
+    center_text.width = SCREEN_WIDTH * .25
+    center_text.centerx = GRID_DISPLAY.get_rect().centerx
+    center_text.centery = SCREEN_HEIGHT * .8
+    back_button = Button(center_text, "Back", GRAY, BUTTON_FONT)
+    all_button.append(back_button)
 
+    # continue button
+    center_text = pygame.Rect.copy(center_text)
+    center_text.centery = SCREEN_HEIGHT * .75
+    center_text.centerx = SCREEN_WIDTH * .5
+    pause_button = Button(center_text, "Continue", GRAY, BUTTON_FONT)
+    all_button.append(pause_button)
+
+    # reset button
+    center_text = pygame.Rect.copy(center_text)
+    center_text.centery = SCREEN_HEIGHT * .88
+    center_text.centerx = SCREEN_WIDTH * .5
+    reset_button = Button(center_text, "Reset", GRAY, BUTTON_FONT)
+    all_button.append(reset_button)
+
+    return all_button
+
+
+'''You've reached the end of the file?'''
 main()
